0d738f9cfe55fbc627acee52d0f7e1475be22f74
tsaren99 <tsarenkovslava@yandex.ru>
2024-05-22 12:03:23 +0300

    add service discovery

--- bank-processor/src/main/java/com/tsarankou/bankprocessor/BankProcessorApplication.java
+++ bank-processor/src/main/java/com/tsarankou/bankprocessor/BankProcessorApplication.java
@@ -2,8 +2,10 @@ package com.tsarankou.bankprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BankProcessorApplication {
     public static void main(String[] args) {
        SpringApplication.run(BankProcessorApplication.class);
